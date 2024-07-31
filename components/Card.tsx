aaf3a53ca0ff6100b5c550f7bbc976e6c68271a0
Saurabh Raj <167332055+Saurabh87090@users.noreply.github.com>
2024-07-31 20:07:58 +0530

    Update Card.tsx

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,6 +5,7 @@ function Card(props: any) {
     const { name, imgSrc, content, star } = props;
     return (
         <div className='p-4 w-full sm:w-1/2 md:w-2/2 lg:w-1/4'>
+            
             <div className='flex flex-col items-center'>
                 <img className='w-32 h-32' src={imgSrc} alt={name} />
                 <h1 className='mt-4 text-[20px] font-semibold w-[160px] text-center'>{name}</h1>
