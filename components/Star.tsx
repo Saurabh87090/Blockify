098b934053ebfcb838572ac3de0687cf24b50d3a
Saurabh Raj <167332055+Saurabh87090@users.noreply.github.com>
2025-01-06 08:31:44 +0530

    Update Star.tsx

--- components/Star.tsx
+++ components/Star.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 function Star({ star }: { star: number }) {
   return (
-    
     <div className='flex space-x-1 mx-[130px] my-4 w-[140px]'>
       {Array.from({ length: star }, (_, index) => (
         <img
