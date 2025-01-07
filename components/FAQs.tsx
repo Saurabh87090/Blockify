cb0627c2100e42ec26d2bc7f6240381597b2d7b4
Saurabh Raj <167332055+Saurabh87090@users.noreply.github.com>
2025-01-07 08:20:52 +0530

    Update FAQs.tsx

--- components/FAQs.tsx
+++ components/FAQs.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const FAQs = () => {
   return (
-    
     <div>
       <section className="flex items-center justify-center py-10 bg-gray-50 sm:py-16 lg:py-24">
         <div className="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
