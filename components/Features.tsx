7bd192c0fa68cbd542a56df6bf67bf3a80a6fdaa
Saurabh Raj <167332055+Saurabh87090@users.noreply.github.com>
2025-02-23 09:49:02 +0530

    Update Features.tsx

--- components/Features.tsx
+++ components/Features.tsx
@@ -4,7 +4,6 @@ const Features = () => {
   return (
     <div>
       <div>
-        
         {/* Brand Images */}
         <div className="  grid grid-cols-2 gap-8 p-4 sm:grid-cols-3 lg:grid-cols-6 lg:mt-[60px]">
           <img className="mx-auto mt-4" src="shopify.svg" alt="shopify" />
